7ce5dd4658c2db8f6e3943cc0dd581a03633fd04
remus Leonte <rleonte@fitbit.com>
2020-07-01 23:31:43 -0700

    added new stickers

--- SignalServiceKit/src/Messages/Stickers/DefaultStickers.swift
+++ SignalServiceKit/src/Messages/Stickers/DefaultStickers.swift
@@ -21,9 +21,11 @@ class DefaultStickerPack {
     private class func parseAll() -> [StickerPackInfo: DefaultStickerPack] {
         let packs = [
             // AIVAN
-            DefaultStickerPack(packIdHex: "c476059cb15a7660fbbe075332fd8619", packKeyHex: "cc95ecfbe523643ec131c15b68b20be2bcacf25615f37027b5358c7982907310", shouldAutoInstall: true)!,
+            DefaultStickerPack(packIdHex: "c476059cb15a7660fbbe075332fd8619", packKeyHex: "cc95ecfbe523643ec131c15b68b20be2bcacf25615f37027b5358c7982907310", shouldAutoInstall: true)!
             // SANTALINA
             DefaultStickerPack(packIdHex: "1c03b91dee7e8679203c3dbed97b440b", packKeyHex: "e5198b58f634fb255269eef2891d4859179be380cd42e048f77a3a9b7da42035", shouldAutoInstall: true)!
+            // FELINA
+            DefaultStickerPack(packIdHex: "c2aa13de9a36acdb3cef4de48c22e4e2", packKeyHex: "7c973ff3dd64832332e2fa1edf3555bdeb008b13bde73f52fc9e9e815430141c", shouldAutoInstall: true)!
         ]
         var result = [StickerPackInfo: DefaultStickerPack]()
         for pack in packs {
